6e97c5d6de30a8aea735c2ed639a9d5aa4e12006
Rohit Das boby <167709785+rohitraz90@users.noreply.github.com>
2026-09-23 15:43:42 +0900

    Update API_BASE URL in script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "https://project-file-7x7n.onrender.com";
+  const API_BASE = "https://project-file-1.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
